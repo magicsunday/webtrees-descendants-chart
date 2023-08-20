e0e3a7d3b25c224351d8727d07dfd19f8114a729
Rico Sonntag <mail@ricosonntag.de>
2023-08-20 16:49:42 +0200

    Release 1.4.1

--- src/Module.php
+++ src/Module.php
@@ -59,7 +59,7 @@ class Module extends DescendancyChartModule implements ModuleCustomInterface
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '1.4.1-dev';
+    public const CUSTOM_VERSION = '1.4.1';
 
     /**
      * @var string
