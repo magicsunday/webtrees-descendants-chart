0f7d581b24fca1ed82f542b781228d85cdac7f9f
Rico Sonntag <mail@ricosonntag.de>
2026-05-02 16:24:13 +0200

    Bump version to 2.4.1-dev

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends DescendancyChartModule implements ModuleAssetUrlInterface, 
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.4.0';
+    public const CUSTOM_VERSION = '2.4.1-dev';
 
     /**
      * @var string
