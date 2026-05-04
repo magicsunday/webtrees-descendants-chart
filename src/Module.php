dabe64c6310284d1639a9b046afd944babeb7bfa
Rico Sonntag <mail@ricosonntag.de>
2026-05-04 12:23:50 +0200

    Release 2.6.0

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends DescendancyChartModule implements ModuleAssetUrlInterface, 
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.5.2-dev';
+    public const CUSTOM_VERSION = '2.6.0';
 
     /**
      * @var string
